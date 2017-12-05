c2b3dc1f0a4aae4c36501a6cf686f1db3572d0a0
Jonathan Weber <jw@qloc.de>
2017-12-05 22:36:08 +0100

    added various sensors

--- modules/HomematicIP.py
+++ modules/HomematicIP.py
@@ -17,7 +17,16 @@ def init(c = []):
     metrics['heating'] = {}
     metrics['heating']['setPointTemperature'] = Gauge('homematicip_heating_setpointtemperature_celsius', 'Homematic IP setpoint temperature of a room', ['id', 'room'])
     metrics['heating']['actualTemperature'] = Gauge('homematicip_heating_actualtemperature_celsius', 'Homematic IP actual temperature of a room', ['id', 'room'])
+    metrics['heating']['boostmode'] = Gauge('homematicip_heating_boostmode', 'Homematic IP wether boost mode is on or off', ['id', 'room'])
+    metrics['heating']['ecomode'] = Gauge('homematicip_heating_ecomode', 'Homematic IP wether eco mode is on or off', ['id', 'room'])
+    metrics['heating']['partymode'] = Gauge('homematicip_heating_partymode', 'Homematic IP wether party mode is on or off', ['id', 'room'])
+    metrics['heating']['humidity'] = Gauge('homematicip_heating_humidity_percent', 'Homematic IP current relative humidity in percent', ['id', 'room'])
 
+    metrics['heating']['lowbattery'] = Gauge('homematicip_heating_lowbattery', 'Homematic IP wether the battery is low or not', ['id', 'name', 'room', 'devicetype', 'modeltype'])
+    metrics['heating']['reachable'] = Gauge('homematicip_heating_reachable', 'Homematic IP wether the device is reachable or not', ['id', 'name', 'room', 'devicetype', 'modeltype'])
+
+    metrics['heating']['valveposition'] = Gauge('homematicip_heating_valveposition', 'Homematic IP position of the valve', ['id', 'name', 'room', 'modeltype'])
+    metrics['heating']['windowState'] = Gauge('homematicip_heating_windowstate', 'Homematic IP wether the window is open or closed', ['id', 'name', 'room', 'modeltype'])
 
 def getdata():
     global config
@@ -37,3 +46,45 @@ def getdata():
                 if g.groupType == 'HEATING':
                     heating['setPointTemperature'].labels(id=g.id, room=g.label).set(g.setPointTemperature)
                     heating['actualTemperature'].labels(id=g.id, room=g.label).set(g.actualTemperature)
+                    heating['humidity'].labels(id=g.id, room=g.label).set(g.humidity)
+
+                    if g.windowState == 'CLOSED':
+                        heating['windowState'].labels(id=g.id, room=g.label, name=g.label, modeltype='ROOM').set(0)
+                    else:
+                        heating['windowState'].labels(id=g.id, room=g.label, name=g.label, modeltype='ROOM').set(1)
+
+                    if g.boostMode:
+                        heating['boostmode'].labels(id=g.id, room=g.label).set(1)
+                    else:
+                        heating['boostmode'].labels(id=g.id, room=g.label).set(0)
+
+                    if g.controlMode == 'ECO':
+                        heating['ecomode'].labels(id=g.id, room=g.label).set(1)
+                    else:
+                        heating['ecomode'].labels(id=g.id, room=g.label).set(0)
+
+                    if g.partyMode:
+                        heating['partymode'].labels(id=g.id, room=g.label).set(1)
+                    else:
+                        heating['partymode'].labels(id=g.id, room=g.label).set(0)
+
+                    for d in g.devices:
+                        if d.lowBat:
+                            heating['lowbattery'].labels(id=g.id, name=d.label, room=g.label, devicetype=d.deviceType, modeltype=d.modelType).set(1)
+                        else:
+                            heating['lowbattery'].labels(id=g.id, name=d.label, room=g.label, devicetype=d.deviceType, modeltype=d.modelType).set(0)
+
+                        if d.unreach:
+                            heating['reachable'].labels(id=g.id, name=d.label, room=g.label, devicetype=d.deviceType,
+                                                         modeltype=d.modelType).set(0)
+                        else:
+                            heating['reachable'].labels(id=g.id, name=d.label, room=g.label, devicetype=d.deviceType,
+                                                         modeltype=d.modelType).set(1)
+
+                        if d.deviceType == 'HEATING_THERMOSTAT':
+                            heating['valveposition'].labels(id=g.id, name=d.label, room=g.label, modeltype=d.modelType).set(d.valvePosition)
+                        elif d.deviceType == 'SHUTTER_CONTACT':
+                            if d.windowState == 'CLOSED':
+                                heating['windowState'].labels(id=g.id, room=g.label, name=d.label, modeltype=d.modelType).set(0)
+                            else:
+                                heating['windowState'].labels(id=g.id, room=g.label, name=d.label, modeltype=d.modelType).set(1)
